7e87f6c2f22e7a4358868e28c9123f35f93956b4
TroAlexis <aleksei.troshin1997@gmail.com>
2020-10-12 17:59:57 +0300

    Reworked scss structure, added some useful mixins.

--- src/entries/index/index.js
+++ src/entries/index/index.js
@@ -1,4 +1,5 @@
 // WEBPACK ENTRY FILE MAIN
+
 // ASYNC AWAIT SUPPORT
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -6,7 +7,7 @@ import 'regenerator-runtime/runtime';
 // JS here
 
 // SCSS
-import 'Scss/shared.scss';
+import 'Scss/global.scss';
 
 if (module.hot) {
   module.hot.accept();
