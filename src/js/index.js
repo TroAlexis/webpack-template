415d662ae7b5b9ebb25b5696eaa74bbcf4307532
TroAlexis <aleksei.troshin1997@gmail.com>
2021-02-20 18:02:10 +0300

    Added slider init example

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
 // Main js file
+
+// SLIDER EXAMPLE
+// $(document).ready(() => {
+//   const settings = {
+//     dots: true,
+//     arrows: false,
+//     mobileFirst: true,
+//     adaptiveHeight: true,
+//     slidesToShow: 1,
+//     responsive: [
+//       {
+//         breakpoint: 767,
+//         settings: 'unslick',
+//       },
+//     ],
+//   };
+//   const advantagesCards = $('.proof__cards');
+//   const reviewsCards = $('.reviews__content');
+//
+//   const sliders = [
+//     advantagesCards.slick(settings),
+//     reviewsCards.slick(settings),
+//   ];
+//
+//   $(window).on('resize', () => {
+//     if ($(window).width() < 768) {
+//       sliders.forEach((slider) => {
+//         if (!isSliderInitialized(slider)) {
+//           slider.slick(settings);
+//         }
+//       });
+//     }
+//   });
+//
+//   function isSliderInitialized(slider) {
+//     return slider.hasClass('slick-initialized');
+//   }
+// });
