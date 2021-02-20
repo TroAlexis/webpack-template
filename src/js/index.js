7c1f90fb12b78df902d83e8cbde363b2e18331fb
TroAlexis <aleksei.troshin1997@gmail.com>
2021-02-20 18:45:51 +0300

    Added blocks, components, sections to pug includes

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,37 @@
 // Main js file
+$(document).ready(() => {
+  const settings = {
+    dots: true,
+    arrows: false,
+    mobileFirst: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: 'unslick',
+      },
+    ],
+  };
+  const advantagesCards = $('.proof__cards');
+  const reviewsCards = $('.reviews__content');
 
-// SLIDER EXAMPLE
-// $(document).ready(() => {
-//   const settings = {
-//     dots: true,
-//     arrows: false,
-//     mobileFirst: true,
-//     adaptiveHeight: true,
-//     slidesToShow: 1,
-//     responsive: [
-//       {
-//         breakpoint: 767,
-//         settings: 'unslick',
-//       },
-//     ],
-//   };
-//   const advantagesCards = $('.proof__cards');
-//   const reviewsCards = $('.reviews__content');
-//
-//   const sliders = [
-//     advantagesCards.slick(settings),
-//     reviewsCards.slick(settings),
-//   ];
-//
-//   $(window).on('resize', () => {
-//     if ($(window).width() < 768) {
-//       sliders.forEach((slider) => {
-//         if (!isSliderInitialized(slider)) {
-//           slider.slick(settings);
-//         }
-//       });
-//     }
-//   });
-//
-//   function isSliderInitialized(slider) {
-//     return slider.hasClass('slick-initialized');
-//   }
-// });
+  const sliders = [
+    advantagesCards.slick(settings),
+    reviewsCards.slick(settings),
+  ];
+
+  $(window).on('resize', () => {
+    if ($(window).width() < 768) {
+      sliders.forEach((slider) => {
+        if (!isSliderInitialized(slider)) {
+          slider.slick(settings);
+        }
+      });
+    }
+  });
+
+  function isSliderInitialized(slider) {
+    return slider.hasClass('slick-initialized');
+  }
+});
